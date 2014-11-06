973c894479922ae1d9cd2e27ca2771e30e7734a2
dyep49 <dyep49@gmail.com>
2014-11-06 16:20:59 -0500

    moves logic to app

--- gulp/utils/libs.js
+++ gulp/utils/libs.js
@@ -1,6 +1,5 @@
 var libs = [
   'd3',
-  'jquery',
   'topojson',
   'queue-async',
   'underscore',
--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,10 +1,8 @@
-var $ = require('jquery');
 var d3 = require('d3');
 d3.tip = require('./vendor/d3-tip')(d3);
 var queue = require('queue-async');
 var topojson = require('topojson');
 var _ = require('underscore');
-
 
 module.exports = function() {
   var width = 960,
@@ -33,36 +31,40 @@ module.exports = function() {
       .attr("class", "background")
       .attr("width", width)
       .attr("height", height)
-      .on("click", clicked);
+      .on("click", clicked)
 
   var g = svg.append("g");
 
   queue()
-      .defer(d3.json, 'data/us_and_races.json')
+      .defer(d3.json, 'data/us.json')
+      .defer(d3.json, 'data/updated_senate_by_county.json')
+      // .defer(d3.json, 'data/us_and_races.json')
       .await(ready);
 
-  function ready(error, us) {
+  function ready(error, us, racesArray) {
+    races = racesArray.races
 
     g.append('g')
         .attr('class', 'counties')
       .selectAll('path')
-        .data(us)
+        .data(addRacesToUs(us, races))
       .enter().append('path')
         .attr('class', 'county')
         .attr('d', path)
         .style('stroke', function(d) {
           return d.race && d.race.length > 0 ? '#FEF' : '#FEF'
         })
+        // .style('fill', 'url(#crosshatch)')
         .style('fill', setFill)
         // .on('mouseover', function(d) {
-        //   console.log(d.race[0])
+        //   console.log(d)
         // })
         .on('mouseover', tip.show)
         .on('mouseout', tip.hide)
-        .on('click', clicked)
+        .on('click', clicked);
 
     // svg.append('path')
-    //     .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+    //     .datum(topojson.mesh(states, states.objects.states, function(a, b) { return a !== b; }))
     //     .attr('class', 'states')
     //     .attr('d', path);
   }
@@ -70,7 +72,7 @@ module.exports = function() {
   function setFill(d) {
     if(d.race[0] && d.race[0].reportingUnits) {
       var winner = getWinner(d);
-      return partyScale(winner.party);
+      return winner.party ? partyScale(winner.party) : 'url(#crosshatch)';
     } else {
       return 'white'
     }
@@ -116,6 +118,21 @@ module.exports = function() {
     return '<span class="winner-name">' + winnerName + '</span>' + '<span style="color:' + partyScale(winner.party) + '">' + winner.party + '</span>'
   }
 
+  function getReportingUnitFromFipsCode(races, fipsCode) {
+    return races.filter(function(race) {
+      return race.reportingUnits[0].fipsCode === fipsCode
+    })
+  } 
+
+  function addRacesToUs(us, races) {
+    var features = topojson.feature(us, us.objects.counties).features
+
+    return features.map(function(feature) {
+      feature.race = getReportingUnitFromFipsCode(races, feature.id.toString())
+      return feature
+    })
+
+  }
 
   d3.select(self.frameElement).style('height', height + 'px');
 }
