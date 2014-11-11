e9de0edd98344f7decaf623832db565ffcef687a
dyep49 <dyep49@gmail.com>
2014-11-11 18:45:59 -0500

    adding votes to tooltip

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -215,7 +215,7 @@ module.exports = function() {
 
     return '<span class="winner-name">' + winner.name + '</span>'
       + '<span style="color:' + b3.partyScale(winner.party) + '">' + winner.party + '</span> '
-      + '<span class="votes">' + d3.format(",")(winner.voteCount) + '</span>';
+      + '<span class="votes">' + d3.format(",")(winner.voteCount) + ' votes</span>';
   }
 
 
